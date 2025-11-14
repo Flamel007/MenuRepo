f7b9c6a4a4b9def38f132509f9b0b07f897816e9
Flamel007 <flamelblackdeath007@gmail.com>
2025-11-14 10:51:23 -0300

    Adicionado Inserir

--- src/ClassePrincipal.java
+++ src/ClassePrincipal.java
@@ -22,10 +22,42 @@ public class ClassePrincipal {
 		int opcao;
 		
 		do{
-			
+			limparTela();
 			System.out.println("MENU DE OPÇÃO");
+			System.out.println("[1] Adicionar um aluno");
+			System.out.println("[2] Excluir um aluno");
+			System.out.println("[3] Ver alunos");
+			System.out.println("[4] SAIR");
 			
-		}while(opcao != 3);
+			opcao = sc.nextInt();
+			sc.nextLine();
+			
+			limparTela();
+			
+			switch(opcao){
+			
+			case 1:
+				System.out.println("Selecionado opção 1");
+				System.out.println("Informe o nome:");
+				String nome = sc.nextLine();
+				System.out.println("Informe o CPF:");
+				String cpf = sc.nextLine();
+				System.out.println("Informe o curso:");
+				String curso = sc.nextLine();
+				System.out.println("Cadastra nome, cpf e curso");
+				break;
+			case 2:
+				System.out.println("Selecionado opção 2");
+			case 3:
+				System.out.println("Selecionado opção 3");
+				default:
+					System.out.println("Opção Padrão");
+			
+			
+			}
+			
+			
+		}while(opcao != 4);
 	}
 
 }
